b5d4af05dcd88500cf2939ecc61c507df3c6f331
James Young <jayoung@pivotal.io>
2016-03-16 15:05:54 -0700

    wip
    Signed-off-by: Jim Park <spark@pivotal.io>

--- spec/template_helper_spec.rb
+++ spec/template_helper_spec.rb
@@ -1,12 +1,13 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe TemplateHelper  do
-  describe ".templates_for" do
-    let(:template_type) { "template_helper" }
-    let(:env) { "testing_env" }
-    let(:workingdirectory) { File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))}
-    subject() {TemplateHelper.templates_for(template_type, env, workingdirectory)}
+  let(:template_type) { 'template_helper' }
+  let(:env) { 'testing_env' }
+  let(:workingdirectory) { File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))}
+  let(:config_for_env) { { } }
+  subject(:template_helper) {TemplateHelper.templates_for(template_type, env, workingdirectory, config_for_env)}
 
+  describe '.templates_for' do
     it { is_expected.to match_array(
         [
           /shared\/a.json.erb/,
@@ -15,9 +16,55 @@ describe TemplateHelper  do
         ]
       )
     }
-    # we expect items in `shared` to always be included
+  end
 
-    # we'll go over the folders in `tags` and check them...
+  describe '.find_templates' do
+    subject() { TemplateHelper.find_templates(workingdirectory, 'template_helper_templates', 'testing_env') }
+    it { is_expected.to match_array([/c.json.erb/]) }
+  end
+
+
+  describe '.expand_path' do
+    subject() { TemplateHelper }
+    it { expect(subject.expand_path('a', 'b')).to eq(File.join('a', 'b')) }
+    it { expect(subject.expand_path('a')).to eq(File.join('a')) }
+  end
+
+  describe 'tagged environments' do
+    context 'tag_a' do
+      let(:config_for_env) { { 'tags' => [ 'tag_a' ] } }
+
+      describe '.templates_for' do
+        subject() { TemplateHelper.templates_for(template_type, env, workingdirectory, config_for_env) }
+        it { is_expected.to match_array(
+          [
+            /shared\/a.json.erb/,
+            /shared\/b.json.erb/,
+            /testing_env\/c.json.erb/,
+            /tags\/tag_a\/1.json.erb/
+          ]
+        )
+        }
+      end
+    end
+    context 'tag_a and tag_b' do
+      let(:config_for_env) { { 'tags' => [ 'tag_a', 'tag_b' ] } }
+
+      describe '.templates_for' do
+        subject() { TemplateHelper.templates_for(template_type, env, workingdirectory, config_for_env) }
+        it { is_expected.to match_array(
+          [
+            /shared\/a.json.erb/,
+            /shared\/b.json.erb/,
+            /testing_env\/c.json.erb/,
+            /tags\/tag_a\/1.json.erb/,
+            /tags\/tag_b\/2.json.erb/,
+            /tags\/tag_b\/3.json.erb/
+          ]
+        )
+        }
+      end
+    end
   end
 
 
