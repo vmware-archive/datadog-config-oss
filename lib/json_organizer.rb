4c95d86d2b0f16a858bb2a51cff0fd7426b30984
Nolan Varani <nvarani@pivotal.io>
2016-04-07 15:51:51 -0700

    Added json_organizer library to alphabetize JSON
    [#116178177](https://www.pivotaltracker.com/story/show/116178177)

---
+++ lib/json_organizer.rb
@@ -0,0 +1,25 @@
+require 'json'
+
+class Hash
+  def sort_recursive(&block)
+    self.keys.sort(&block).reduce({}) do |seed, key|
+      seed[key] = self[key]
+      if seed[key].is_a?(Hash)
+        seed[key] = seed[key].sort_recursive(&block)
+      elsif seed[key].is_a?(Array)
+        seed[key].each do |item|
+          if item.is_a?(Hash)
+            item = item.sort_recursive
+          end
+        end
+        seed[key].sort! { |x,y| JSON.generate(x) <=> JSON.generate(y) }
+      end
+      seed
+    end
+  end
+end
+
+def sort_json(json)
+  jsonHash = JSON.parse(json)
+  return JSON.generate(jsonHash.sort_recursive)
+end
