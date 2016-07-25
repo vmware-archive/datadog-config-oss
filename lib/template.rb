c825c3455f426b21977b5226536241e229c22512
James Young <jayoung@pivotal.io>
2016-07-25 15:13:06 -0700

    Refactoring templates to a separate class
    Signed-off-by: Jim Park <spark@pivotal.io>

--- lib/template.rb
+++ lib/template.rb
@@ -1,2 +1,35 @@
+require "erb"
+require "erb_context"
+
 class Template
+  def initialize(params)
+    @template_file = params.fetch(:template_file)
+    @search_and_replace = params.fetch(:search_and_replace)
+  end
+
+  def to_erb_from_string(str)
+    @search_and_replace.each do |k,v|
+      str.gsub!(Regexp.new(v), "<%= #{k} %>" )
+    end
+
+    str
+  end
+
+  def to_string_from_erb(str)
+    # this returns the binding as it exists from the context's perspective
+    # so all of the properties set on the ErbContext instance are exposed as
+    # variables in the binding and therefore the ERB
+    # TL;DR Magic.
+    context = ErbContext.new(@search_and_replace)
+    context_binding = context.instance_eval { binding }
+    erb = ERB.new(str)
+    output = erb.result(context_binding)
+
+    return output
+
+    #thresholds_file_path = thresholds_file(template_path)
+    # if File.exists? thresholds_file_path
+    #   context.thresholds = thresholds_for_yaml_file(thresholds_file_path)
+    # end
+  end
 end
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,8 +1,84 @@
 require 'rspec'
 
 describe Template do
-  subject(:template) {described_class.new()}
+  let(:template_file)  {Tempfile.new('deleteme')}
+  let(:search_and_replace) {
+    {
+      key: 'value',
+      diego_deployment: 'some-deployment-diego',
+      deployment: 'some-deployment',
+      bosh_deployment: 'some-bosh-deployment',
+    }
+  }
+
+  subject(:template) {described_class.new(
+    template_file: template_file,
+    search_and_replace: search_and_replace
+  )}
+
+  after(:each) { FileUtils.rm(template_file)}
 
   it { is_expected.to be_a Template }
+
+  describe '#new' do
+    describe 'instance_variables' do
+      describe '@template_file' do
+        subject() { template.instance_variable_get(:@template_file) }
+        it { is_expected.to match template_file }
+      end
+
+      describe '@search_and_replace' do
+        subject() { template.instance_variable_get(:@search_and_replace) }
+        it { is_expected.to match search_and_replace }
+      end
+
+    end
+  end
+
+  describe '#raw' do
+
+  end
+
+
+  describe '#to_datadog' do
+    xit 'looks like json' do
+
+    end
+
+    describe '#to_string_from_erb' do
+      it "convert values from erb vars" do
+        expect(template.to_string_from_erb("<%= deployment %>")).to eq("some-deployment")
+        expect(template.to_string_from_erb("<%= bosh_deployment %>")).to eq("some-bosh-deployment")
+        expect(template.to_string_from_erb( "<%= deployment %> <%= diego_deployment %>")).to eq("some-deployment some-deployment-diego")
+      end
+    end
+
+  end
+
+
+  describe '#from_datadog' do
+    it 'receives a json object from (purportedly datadog)' do
+
+    end
+
+    describe '#to_erb_from_string' do
+      it "convert values back to erb vars" do
+        expect(template.to_erb_from_string("some-deployment")).to eq("<%= deployment %>")
+        expect(template.to_erb_from_string("some-bosh-deployment")).to eq("<%= bosh_deployment %>")
+      end
+
+      xit 'supports regex strings' do
+        expect(template.to_erb_from_string("some-deployment")).to eq("<%= deployment %>")
+        expect(template.to_erb_from_string("datadog.nozzle.asdf: { deployment: some-deployment }")).to
+          eq("datadog.nozzle.asdf: { deployment: <%= metron_agent_deployment %> }")
+      end
+
+      it "can handle value overlaps" do
+        expect(template.to_erb_from_string("some-deployment some-deployment-diego")).to eq(
+            "<%= deployment %> <%= diego_deployment %>")
+      end
+    end
+
+  end
 
 end
